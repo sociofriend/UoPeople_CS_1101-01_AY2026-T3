4d86eb26dba8beb59cd6764a3ddac771ce97555e
sociofriend <lilit_avdalyan@yahoo.com>
2026-03-04 08:27:33 +0400

    feat: implement task 2 and 3 of the Programming Assignment Unit 5

--- Programming_Assignment_Unit_5.py
+++ Programming_Assignment_Unit_5.py
@@ -5,7 +5,7 @@ Task 2. Count the number of vowels.
 Task 3. Reverse it.
 """
 
-# Task 1
+# region Task 1. Display n characters from left
 def get_name():
     myName = input("Enter your name: ")
     for c in myName:
@@ -15,18 +15,17 @@ def get_name():
             return get_name()
     return myName
 
-
-def get_number_of_characters(name):
-    number_of_charactes_from_left = input("Please enter the number of characters you'd like to see from the left side of the string: ")
+def get_number_of_characters_for(name):
+    number_of_charactes_from_left = input("How many first letters to preview: ")
     try:
         converted_number = int(number_of_charactes_from_left)
-    except ValueError:
+    except Exception as e:
         print("Please enter a valid integer.")
-        return get_number_of_characters()
+        return get_number_of_characters_for(name)
 
     if converted_number > len(name):
         print(f"There are only {len(name)} characters in the string.")
-        return get_number_of_characters(name)
+        return get_number_of_characters_for(name)
     else:
         return converted_number
 
@@ -35,15 +34,64 @@ def get_characters_from_left(n, name):
         print("Number of charachters of the name and 'n' shall be greater than zero.")
 
     if n > len(name):
-        print(f"{n} is grater than number of character of {name}")
+        print(f"{n} is grater than number of character of '{name}'")
         return get_characters_from_left(n, name)
 
-    print(name[:n])
-    return name[:n]
+    substring = name[:n]
+    print(f"First {n} character/s of '{name}' is/are: '{substring}'")
+    return substring
+# endregion
 
+# region Task 2. Count the number of vowels
+def is_vowel(c):
+    try:
+        vowels =['a', 'e', 'i', 'o', 'u']
+        return c.lower() in vowels
+    except Exception as e:
+        print(f"An error occurred: {e}. Please try again.")
+        return False
 
+def count_vowels(name):
+    count = 0
+    for c in name:
+        if is_vowel(c):
+            count += 1
+    print(f"There are {count} vowels in name '{name}'")
+    return count
+# endregion
+
+# region Task 3. Reverse it
+def reverse_name(name):
+    len_name = len(name)
+    if len_name == 0:
+        print("Empty string.")
+        return ""
+
+    reversed_name = ""
+
+    for i in range(len_name):
+        reversed_name += name[len_name - 1 - i]
+    print(f"The reversed string of '{name}' is: '{reversed_name}'")
+    return reversed_name
+# endregion
+
+# region Execution
+# get name
 safe_name = get_name()
-safe_number = get_number_of_characters(safe_name)
-# print(safe_name, safe_number)
+
+# get number for left side preview
+print("\n")
+safe_number = get_number_of_characters_for(safe_name)
+
+# preview a given number of characters from left to right side of the string
+print("\n")
 get_characters_from_left(safe_number, safe_name)
 
+# count vowels of the name
+print("\n")
+count_vowels(safe_name)
+
+# revers name and print it
+print("\n")
+reverse_name(safe_name)
+# endregion
